ff2e093789c5a124fdc46460c38fdeeb9cc4ab69
etud6-3 <etud6-3@ip-10-0-26-26.ec2.internal>
2024-01-25 10:07:11 +0100

    telemac build files

---
+++ configs/pysource.gfortran.sh
@@ -0,0 +1,27 @@
+# This file is a Linux environment file template
+# Sourcing it will position all the environment variables required
+# to compile and run TELEMAC
+# You only need to replace <your-systel-file> and <your-config>
+# to reflect your own configuration
+#
+# Path to TELEMAC root directory
+export HOMETEL=$HOME/dev/hackaton-hpc-telemac
+# Configuration file
+export SYSTELCFG=$HOMETEL/configs/systel.cfg
+# Name of the configuration to use
+export USETELCFG=gfortran
+# Path to this file
+export SOURCEFILE=$HOMETEL/configs/pysource.gfortran.sh
+# Add TELEMAC Python scripts to PATH
+export PATH=$HOMETEL/scripts/python3:.:$PATH
+# Add TELEMAC libraries to LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$HOMETEL/builds/$USETELCFG/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$HOMETEL/builds/$USETELCFG/wrap_api/lib:$LD_LIBRARY_PATH
+# Add TELEMAC Python scripts and extension modules to PYTHONPATH
+export PYTHONPATH=$HOMETEL/scripts/python3:$PYTHONPATH
+export PYTHONPATH=$HOMETEL/builds/$USETELCFG/wrap_api/lib:$PYTHONPATH
+# Force Python to flush its output
+export PYTHONUNBUFFERED='true'
+
+### METIS -------------------------------------------------------------
+export METISHOME=~/dev/hackaton-hpc-telemac/optionals/metis
